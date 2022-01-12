56507b3ae89b2e5ffc0748847aa7e42e6071d903
Catherine Kuhn <31172559+catherinekuhn17@users.noreply.github.com>
2022-01-12 03:03:04 -0800

    Update filter_partial_occupancy.py

--- filter_partial_occupancy.py
+++ filter_partial_occupancy.py
@@ -12,17 +12,14 @@ with open(pdb_ids_fn) as infile, open(outfile, 'w') as outfile:
         structure = parser.get_structure(pdb_id, pdb_fn)
 
         atoms = structure.get_atoms()
-        partial_occupancy = False
         for a in atoms:
             if 'HOH' in str(a.get_parent()):
                 if a.get_occupancy()<1:
-                    partial_occupancy = True
-        if partial_occupancy:
-            outfile.write(pdb_id+"\n")
-            os.mkdir(os.path.join('partial_occupancy',pdb_id))
-            file_names = os.listdir(os.path.join('all',pdb_id))
-            for file_name in file_names:
-                shutil.move(os.path.join('all', pdb_id, file_name), os.path.join('partial_occupancy', pdb_id, file_name))
+                    outfile.write(pdb_id+"\n")
+                    os.mkdir(os.path.join('partial_occupancy',pdb_id))
+                    file_names = os.listdir(os.path.join('all',pdb_id))
+                    for file_name in file_names:
+                        shutil.move(os.path.join('all', pdb_id, file_name), os.path.join('partial_occupancy', pdb_id, file_name))
         
 
 
