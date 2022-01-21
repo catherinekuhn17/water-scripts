d97e531a0d0e0d16c5b9f3a1007ea4778e784871
Catherine Kuhn <ckuhn@dev1.wynton.ucsf.edu>
2022-01-20 23:06:31 -0800

    idk

--- filter_partial_occupancy.py
+++ filter_partial_occupancy.py
@@ -1,13 +1,13 @@
 from Bio.PDB import *
 import os
 import shutil
-
-pdb_ids_fn = 'pdb_id_list.txt'
-outfile = 'pdb_id_list_part_occupancy.txt'
+base_folder = '/wynton/home/rotation/ckuhn/Desktop/Fraser_lab/pdb_files/download2/'
+pdb_ids_fn = '/wynton/home/rotation/ckuhn/Desktop/Fraser_lab/pdb_files/download2/pdb_ids_all.txt'
+outfile = '/wynton/home/rotation/ckuhn/Desktop/Fraser_lab/pdb_files/download2/pdb_id_list_part_occupancy.txt'
 with open(pdb_ids_fn) as infile, open(outfile, 'w') as outfile:
     while True:
         pdb_id = infile.readline().rstrip()
-        pdb_fn = os.path.join('all',pdb_id, pdb_id + '.pdb')
+        pdb_fn = os.path.join(base_folder, 'all',pdb_id, pdb_id + '.pdb')
         parser = PDBParser()
         structure = parser.get_structure(pdb_id, pdb_fn)
 
@@ -19,10 +19,10 @@ with open(pdb_ids_fn) as infile, open(outfile, 'w') as outfile:
                     partial_occupancy = True
         if partial_occupancy:
             outfile.write(pdb_id+"\n")
-            os.mkdir(os.path.join('partial_occupancy',pdb_id))
-            file_names = os.listdir(os.path.join('all',pdb_id))
+            os.mkdir(os.path.join(base_folder, 'partial_occupancy',pdb_id))
+            file_names = os.listdir(os.path.join(base_folder, 'all',pdb_id))
             for file_name in file_names:
-                shutil.move(os.path.join('all', pdb_id, file_name), os.path.join('partial_occupancy', pdb_id, file_name))
+                shutil.copy(os.path.join(base_folder, 'all', pdb_id, file_name), os.path.join(base_folder,'partial_occupancy', pdb_id, file_name))
         
 
 
